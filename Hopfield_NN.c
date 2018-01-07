c23177ac19e5f88844e6fd9c9a5d59a0cbe9d99e
Dennis <dennisburkertmx.de>
2018-01-07 15:23:03 +0100

    input read bis testset funktioniert

--- Hopfield_NN.c
+++ Hopfield_NN.c
@@ -3,23 +3,62 @@
 #include <time.h>
 #include <math.h>
 
-int pattern[10][20][10];
-int temp_input = 0;
+int train_pattern[10][20][10];
+char temp_input = 0;
+char temp_last = 0;
+int start = 0;
 int main()
 {
     int i = 0;
     int j = 0;
     int k = 0;
-    while(scanf("%i%i%i\n") !="---")
-    while(scanf(" %i\n") != "-")
+    /* Training input*/
+    while(start == 0)
     {
-        while(scanf(" %i", &temp_input) !="\n")
+        scanf("%c", &temp_input);
+
+
+        if(temp_input == '-' && temp_last != '-')
         {
-            pattern[i][j][k] = temp_input;
+            i++;
+            j = 0;
+            k = 0;
+        }
+        else if(temp_last == '-' && temp_input == '-')
+        {
+            break;
+        }
+        else if(temp_input == '.' || temp_input == '*')
+        {
+            train_pattern[i][j][k] = temp_input;
             j++;
         }
-        k++;
-    }
-    i++;
+        else if(temp_input == '\n')
+        {
+            k++;
+            j = 0;
+        }
 
+        temp_last = temp_input;
+    }
+
+    for(int j= 0; j<10; j++)
+    {
+        for(int k = 0; k<20; k++)
+        {
+            printf("%c", pattern[0][k][j]);
+        }
+        printf("\n");
+    }
+
+    /*test the new patterns*/
+    while(scanf("%c", &temp_input) != EOF)
+    {
+
+    }
+
+
+    return 0;
 }
+
+
