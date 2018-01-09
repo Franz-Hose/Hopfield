9c21caee5c72e180482601b0eb194aaaf866897a
Dennis <dennisburkertmx.de>
2018-01-09 08:38:54 +0100

    28 Punkte, Aufgabe D funktioniert noch nicht, wahrscheinlich muss man random noise hinzufügen

--- Hopfield_NN.c
+++ Hopfield_NN.c
@@ -17,6 +17,7 @@ char temp_last = 0;
 int start = 0;
 double WEIGHTS[n_neurons][n_neurons] = {{0}};
 double out[n_neurons] = {0};
+int bipolarOut[n_neurons] = {0};
 
 void make_train_bipolar()
 {
@@ -84,6 +85,7 @@ void recovery_phase()
     {
         for(int iteration = 0; iteration < max_iter; iteration++)
         {
+            int true_count = 0;
             for(int n1 = 0; n1 < n_neurons; n1++)
             {
                 out[n1] = 0.0;
@@ -93,23 +95,44 @@ void recovery_phase()
                     out[n1] += WEIGHTS[n1][n2] * test_pattern[pattern][n2];
 
                 }
+
+                if(out[n1] >= 0)
+                    bipolarOut[n1] = 1;
+                else
+                    bipolarOut[n1] = -1;
+
+                if(bipolarOut[n1] == test_pattern[pattern][n1])
+                    true_count++;
             }
+
+            if(true_count == n_neurons)
+                break;
+            else
+                for(int neuron = 0; neuron < n_neurons; neuron++)
+                {
+                    test_pattern[pattern][neuron] = bipolarOut[neuron];
+
+                }
         }
-        int true_count = 0;
-        for(int neuron = 0; neuron < n_neurons; neuron++)
+    }
+}
+
+void backtransform_testset()
+{
+    for(int i = 0; i < N_patt; i++)
+    {
+        for(int j= 0; j < n_neurons; j++)
         {
-            if(out[neuron] == test_pattern[pattern][neuron])
-                true_count += 1;
-        }
-        if(true_count == 200)
-            break;
-        else
-            for(int neuron = 0; neuron < n_neurons; neuron++)
+            if(test_pattern[i][j] == 1)
             {
-                test_pattern[pattern][neuron] == out[neuron];
-
+                test_pattern[i][j] = 42;
+            }
+            else if(test_pattern[i][j] ==-1)
+            {
+                test_pattern[i][j] = 46;
             }
 
+        }
     }
 }
 
@@ -143,33 +166,33 @@ int main()
         temp_last = temp_input;
     }
     make_train_bipolar();
-//  //PRINT A PATTERN
-//    for(int j= 0; j<10; j++)
-//    {
-//        for(int k = 0; k<20; k++)
-//        {
-//            printf("%c", train_pattern[0][k][j]);
-//        }
-//        printf("\n");
-//    }
 
 
+    int patterncounter = 0;
+    i = 0;
+    j = 0;
     /*test input with new patterns*/
     while(scanf("%c", &temp_input) != EOF)
     {
-        scanf("%c", &temp_input);
-
-
-        if(temp_input == '-' && temp_last != '-')
-        {
-            i++;
-            j = 0;
-        }
-        else if(temp_input == '.' || temp_input == '*')
+        if(temp_input == '.' || temp_input == '*')
         {
             test_pattern[i][j] = temp_input;
             j++;
+//            if(j == n_neurons) // Auskommentieren für test daheim
+//            {
+//                break;
+//            }
+
+
         }
+        else if(temp_input == '-' && temp_last != '-')
+        {
+            i++;
+            j = 0;
+            patterncounter++;
+
+        }
+
 
         temp_last = temp_input;
     }
@@ -181,5 +204,28 @@ int main()
     /* recover ein schlechtes pattern*/
     recovery_phase();
 
+    backtransform_testset();
+
+    //PRINT A PATTERN
+    int columncounter = 0;
+    j = 0;
+    for(int i = 0; i < N_patt; i++)
+    {
+        for(int j = 0; j < n_neurons; j++)
+        {
+            printf("%c", test_pattern[i][j]);
+            columncounter++;
+            if(columncounter == columns)
+            {
+                columncounter = 0;
+                printf("\n");
+            }
+        }
+        if(i == patterncounter-1)
+        {
+            break;
+        }
+        printf("-\n");
+    }
 
 }
